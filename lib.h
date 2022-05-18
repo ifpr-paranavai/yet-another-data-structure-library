c618f7b9ebf2a0499b0badfb50073591742ae0ef
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-05-18 18:18:54 -0300

    Vector v2
    Code improvements and exception traits; private alloc function; public max_size function; public full function; public insert function correction and overloads; public emplace function correction; public emplace_back function

--- lib.h
+++ lib.h
@@ -1,6 +1,17 @@
 #ifndef __LIB__
 #define __LIB__
 
+#define ENVBITSIZE 64 // bit size of operational system
+// check bit size of operational system
+#if _WIN32 || _WIN64 || __GNUC__
+	#if _WIN64 || __x86_64__ || __ppc64__
+		#define ENV64BIT
+	#else
+		#define ENV32BIT
+		#define ENVBITSIZE 32
+	#endif
+#endif
+
 #define OO_ENCAPSULATE(TYPE, VAR) \
 	private: \
 		TYPE VAR; \
