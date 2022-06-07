4fb93a688d2d4042afdc30f1376c2039d3f29ec1
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-06-07 17:37:11 -0300

    Hash Table
    I started to implement the Hash Table and implemented add operation

---
+++ hash_table.h
@@ -0,0 +1,65 @@
+#ifndef __YADSL_HASH_TABLE__
+#define __YADSL_HASH_TABLE__
+
+#include <cinttypes>
+#include <iostream>
+#include "vector.h"
+#include "list.h"
+#include "lib.h"
+
+using namespace yadsl;
+
+namespace yadsl
+{
+	template<typename T>
+	class hash_table_t 
+	{
+		using value_type = T;
+		using size_type = uint64_t;
+		using index_type = uint64_t;
+		using hash_type = uint64_t;
+
+		private:
+			vector_t<list_t<value_type>> vector;
+			size_type _size = 0;
+
+		private:
+			hash_type hash (value_type value) const noexcept
+			{
+				return value % this->vector.capacity();
+			}
+
+		public:
+			hash_table_t (size_type capacity)
+			{
+				std::cout << "prime:" << next_prime(next_prime(capacity * 2)) << std::endl;
+				this->vector.assign(next_prime(next_prime(capacity * 2)), list_t<value_type>());
+			}
+
+		// capacity
+		public:
+			inline size_type size () const noexcept
+			{
+				return this->_size;
+			}
+			inline bool empty () const noexcept
+			{
+				return this->_size == 0;
+			}
+		
+		// modifiers
+		public:
+			void operator<< (const value_type& value)
+			{
+				this->add(value);
+			} 
+			void add (const value_type& value)
+			{
+				list_t<value_type> *list = &(this->vector[this->hash(value)]);
+				list->push_back(value);
+				this->_size++;
+			}
+	};
+}
+
+#endif
