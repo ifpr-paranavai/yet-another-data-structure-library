ffca599086b0bdc3fe5d64246549624de35490bb
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-10-28 00:48:37 -0300

    fix: namespace

--- pair.h
+++ pair.h
@@ -5,53 +5,57 @@
 #include <cinttypes>
 #include <exception>
 
-template <typename Key, typename T>
-class pair_t
+namespace yadsl
 {
-	using key_type = Key;
-	using value_type = T;
 
-	private:
-		key_type _key;
-		value_type _value;
+	template <typename Key, typename T>
+	class pair_t
+	{
+		using key_type = Key;
+		using value_type = T;
 
-	public:
-		pair_t () {}
-		pair_t (const key_type& key, const value_type& value)
-		{
-			this->_key = key;
-			this->_value = value;
-		}
+		private:
+			key_type _key;
+			value_type _value;
 
-	// element access
-	public:
-		inline key_type& key () noexcept
-		{
-			return this->_key;
-		}
-		inline value_type& value () noexcept
-		{
-			return this->_value;
-		}
-		inline key_type get_key () const noexcept
-		{
-			return this->_key;
-		}
-		inline value_type get_value () const noexcept
-		{
-			return this->_value;
-		}
+		public:
+			pair_t () {}
+			pair_t (const key_type& key, const value_type& value)
+			{
+				this->_key = key;
+				this->_value = value;
+			}
 
-	// element modifier
-	public:
-		void set_key (const key_type& key) noexcept
-		{
-			this->_key = key;
-		}
-		void set_value (const value_type& value) noexcept
-		{
-			this->_value = value;
-		}
-};
+		// element access
+		public:
+			inline key_type& key () noexcept
+			{
+				return this->_key;
+			}
+			inline value_type& value () noexcept
+			{
+				return this->_value;
+			}
+			inline key_type get_key () const noexcept
+			{
+				return this->_key;
+			}
+			inline value_type get_value () const noexcept
+			{
+				return this->_value;
+			}
+
+		// element modifier
+		public:
+			void set_key (const key_type& key) noexcept
+			{
+				this->_key = key;
+			}
+			void set_value (const value_type& value) noexcept
+			{
+				this->_value = value;
+			}
+	};
+}
 
 #endif
